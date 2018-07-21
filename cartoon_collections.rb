b598eb644713d95783c78cefb3580144825c8b08
Jaime Johnsen <drivetraindiva@gmail.com>
2018-07-21 23:19:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,6 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map! {|planeteer_calls| planeteer_calls.capitalize + "!"}
-  planeteer_calls
 end
 
 def long_planeteer_calls(words)
